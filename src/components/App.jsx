f5e39174cfa415ba4fbf75763e1cc5f4ee6851b1
ad3rinto <aad3rinto@gmail.com>
2022-10-03 13:04:13 +0100

    seperate the components

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Card from "./Card";
+import Header from "./Header";
+
+export default function App() {
+	return (
+		<div>
+			<Header />
+			<Card />
+		</div>
+	);
+}
